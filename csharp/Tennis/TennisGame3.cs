3339798838776fcbc5cf3401b4b149825405fe80
Melissa Updike <mellyupd@gmail.com>
2022-11-08 12:41:26 -0500

    improving clarity by implementing naming conventions

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -4,30 +4,30 @@ namespace Tennis
     {
         private int player2Score;
         private int player1Score;
-        private string p1N;
-        private string p2N;
+        private string player1Name;
+        private string player2Name;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
         }
 
         public string GetScore()
         {
-            string s;
+            string score;
             if ((player1Score < 4 && player2Score < 4) && (player1Score + player2Score < 6))
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[player1Score];
-                return (player1Score == player2Score) ? s + "-All" : s + "-" + p[player2Score];
+                string[] scoreNames = { "Love", "Fifteen", "Thirty", "Forty" };
+                score = scoreNames[player1Score];
+                return (player1Score == player2Score) ? score + "-All" : score + "-" + scoreNames[player2Score];
             }
             else
             {
                 if (player1Score == player2Score)
                     return "Deuce";
-                s = player1Score > player2Score ? p1N : p2N;
-                return ((player1Score - player2Score) * (player1Score - player2Score) == 1) ? "Advantage " + s : "Win for " + s;
+                score = player1Score > player2Score ? player1Name : player2Name;
+                return ((player1Score - player2Score) * (player1Score - player2Score) == 1) ? "Advantage " + score : "Win for " + score;
             }
         }
 
